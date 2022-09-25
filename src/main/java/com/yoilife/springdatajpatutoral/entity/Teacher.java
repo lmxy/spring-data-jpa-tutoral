58edaed6214a1d641a6ceae80ac100cf6a03efc6
lmxy <wjzxyaowei@163.com>
2022-09-25 20:32:17 +0800

    complete

--- src/main/java/com/yoilife/springdatajpatutoral/entity/Teacher.java
+++ src/main/java/com/yoilife/springdatajpatutoral/entity/Teacher.java
@@ -1,2 +1,39 @@
-package com.yoilife.springdatajpatutoral.entity;public class Teacher {
+package com.yoilife.springdatajpatutoral.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Teacher {
+
+	@Id
+	@SequenceGenerator(
+			name = "teacher_sequence",
+			sequenceName = "teacher_sequence"
+	)
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "teacher_sequence"
+	)
+	private Long teacherId;
+	private String firstName;
+	private String lastName;
+
+	@OneToMany(
+			cascade = CascadeType.ALL
+	)
+	@JoinColumn(
+			name = "teacher_id",
+			referencedColumnName = "teacherId"
+	)
+	private List<Course> courses;
 }
--- src/main/java/com/yoilife/springdatajpatutoral/repository/TeacherRepository.java
+++ src/main/java/com/yoilife/springdatajpatutoral/repository/TeacherRepository.java
@@ -1,2 +1,10 @@
-package com.yoilife.springdatajpatutoral.repository;public interface TeacherRepository {
+package com.yoilife.springdatajpatutoral.repository;
+
+import com.yoilife.springdatajpatutoral.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+
 }
